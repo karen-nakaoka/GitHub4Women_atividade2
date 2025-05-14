f97a1ace965ed5cedb4dc488fff93e57d99a4519
Karen Nakaoka <144953281+karen-nakaoka@users.noreply.github.com>
2025-05-14 19:13:46 -0300

    Adicionando um arquivo de teste
    Upload do arquivo teste de python para a Atividade 2 do curso GitHub4Women.

---
+++ python_teste.py
@@ -0,0 +1,7 @@
+n = input('Qual é o seu nome? ')
+print(f'Prazer em te conhecer, {n:20}')
+
+n1 = int (input('Digite um número: '))
+n2 = int (input ('Digite outro número: '))
+s = n1 + n2
+print ('A soma de', n1, 'e', n2, 'é', s)
